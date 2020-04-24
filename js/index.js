081656ef4812e5f09456164c62b135987464655e
XuYue <1641692968@qq.com>
2020-04-24 21:55:54 +0800

    提交
    今日作业

--- js/index.js
+++ js/index.js
@@ -1,7 +1,30 @@
-中国站下拉
-$('.hread .chinese-s').click(function(){
-    $('.hread .hread-ul-item').show()
+$('input').mouseenter(function(){
+    $(this).css('padding-left','20px')
 })
-$('.hread .chinese-s').toggleClass(function(){
-    $('.hread .hread-ul-item').show()
+$('input').mouseleave(function(){
+    $(this).css('tranform-X','0px')
+})
+// 中国站下拉
+$('.hread-chinese-s span').mouseenter(function(){
+    $('.hread-chinese-t').show()
+    $('.hread-chinese-s span').css('color','red')
+})
+$('.hread-chinese-s').mouseleave(function(){
+    $('.hread-chinese-t').hide()
+    $('.hread-chinese-s span').css('color','#333333')
+})
+
+
+// 地址替换
+$('.tab-seletor').click(function(){
+    $('.tab-section-two').hide()
+    let index = $(this).index()
+    $('.tab-section-show').eq(index).show()
+    $('.tab-seletor').css('color','#fff')
+})
+$('.tab-seletor-active').click(function(){
+    $('.tab-section-two').show()
+    $('.tab-section-show').hide()
+    let index = $(this).index()
+    $('.tab-section-two').eq(index).hide()
 })
