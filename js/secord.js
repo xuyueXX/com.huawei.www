5ca2c5019579e87f358bdecaf1d2457fca2f4e1c
XuYue <1641692968@qq.com>
2020-04-27 22:13:47 +0800

    提交
    今日作业

---
+++ js/secord.js
@@ -0,0 +1,12 @@
+$('.huaweiclound-item-clound').mouseenter(function(){
+    $('.huaweiclound-hide').show()
+})
+$('.huaweiclound-hide').mouseleave(function(){
+    $('.huaweiclound-hide').hide()
+
+}) 
+$('.huaweiclound-item-clound').mouseleave(function(){
+    $('.huaweiclound-hide').hide()
+})
+
+$('')
