4bea3949d635b409b6f377182863729d835e4bfb
XuYue <1641692968@qq.com>
2020-04-30 21:14:18 +0800

    提交
     今日作业

--- js/secord.js
+++ js/secord.js
@@ -10,3 +10,18 @@ $('.huaweiclound-item-clound').mouseleave(function(){
 })
 
 $('')
+
+$(function(){
+    var isGrey = window.$isGrey = "true";
+    var isBlue = window.$isBlue = "false";
+    var isTitleWhite = window.$isTitleWhite = "false";
+    if(isGrey == "true"){
+        $(".hauweiclound-service").attr("data-bg","grey");
+    }
+    if(isBlue == "true"){
+        $(".service-v6-section-table").attr("data-bg","blue");
+    }
+    if(isTitleWhite == "true"){
+        $(".service-v6-section-table .v6-container").find(".v6-section-head").attr("data-theme","light");
+    }
+})
